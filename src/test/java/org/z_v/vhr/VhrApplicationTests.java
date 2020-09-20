75065f3ee226747ce3077701f3f7d5e965da7473
zzy <17621858881@163.com>
2020-09-20 17:46:09 +0800

    init vhr

---
+++ src/test/java/org/z_v/vhr/VhrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.z_v.vhr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VhrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
